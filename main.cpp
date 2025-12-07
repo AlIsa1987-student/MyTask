fe397c273e810e57166e74600778e7054a362b3b
Яшкин Роман <romanyashkin72@gmail.com>
2025-12-07 20:59:06 +0300

    Добавлен студент 7 и метод поёт

--- main.cpp
+++ main.cpp
@@ -42,6 +42,10 @@ public:
    // метод (Vorsin)
    void dances() {
        cout << FIO << ", " << age << " лет, " << "из группы " << group << " танцует" << endl;
+       
+    // метод (Yashkin)
+   void sings() {
+       cout << FIO << ", " << age << " лет, " << "из группы " << group << " поёт" << endl;
    }
  }
 };
@@ -81,10 +85,11 @@ int main() {
     student5.swims();
     student5.jumping();
 
-    // OBJ 6 - VORSIN
-    Student student6("Иванов Иван", 20, "21318");
-    student6.jumping();
-    student6.dances();
+    
+    // Создание объекта 7 - Yashkin
+    Student student7("Яшкин Роман", 20, "21318");
+    student7.dances();
+    student7.sings();
 
     return 0;
 }
