c21f6977f6560f1d27e280489bfc8dd7917b0860
Егорова Алиса <alisia.eg0rova@yandex.ru>
2025-12-12 10:50:18 +0300

    Возвращаю изменения

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 using namespace std;
 
@@ -9,7 +10,7 @@ public:
     int age;
     string group;
 
-    // Êîíñòðóêòîð ñ ïàðàìåòðàìè
+    // Конструктор с параметрами
     Student(string f, int a, string g) {
         FIO = f;
         age = a;
@@ -17,58 +18,111 @@ public:
     }
 
     void runs() {
-        cout << FIO << ", " << age << " ëåò, " << "èç ãðóïïû " << group << " áåãàåò" << endl;
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " бегает" << endl;
     }
 
     void jumping() {
-        cout << FIO << ", " << age << " ëåò, " << "èç ãðóïïû " << group << " ïðûãàåò" << endl;
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " прыгает" << endl;
     }
 
-    // ìåòîä (wernikeyd)
+    // метод (wernikeyd)
     void pullUP() {
-        cout << FIO << ", " << age << " ëåò, " << "èç ãðóïïû " << group << " ïîäòÿãèâàåòñÿ" <<endl;
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " подтягивается" << endl;
     }
-    //ÐœÐµÑ‚Ð¾Ð´ (Efremov)
+
+    // метод (Efremov)
     void pushUP() {
-    cout << FIO << " ," << age << " Ð»ÐµÑ‚ " << "Ð¸Ð· Ð³Ñ€ÑƒÐ¿Ð¿Ñ‹ " << group << " Ð¾Ñ‚Ð¶Ð¸Ð¼Ð°ÐµÑ‚ÑÑ" << endl;
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " отжимается" << endl;
     }
-   // Ð¼ÐµÑ‚Ð¾Ð´ (Chusovv)
+
+    // метод (Chusovv)
     void crouches() {
-        cout << FIO << ", " << age << " Ð»ÐµÑ‚, " << "Ð¸Ð· Ð³Ñ€ÑƒÐ¿Ð¿Ñ‹ " << group << " Ð¿Ñ€Ð¸ÑÐµÐ´Ð°ÐµÑ‚" << endl;
- }
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " приседает" << endl;
+    }
+
+    // метод (Zverev)
+    void swims() {
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " плавает" << endl;
+    }
+
+    // метод (Vorsin)
+    void dances() {
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " танцует" << endl;
+    }
+
+    // метод (Yashkin)
+    void sings() {
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " поёт" << endl;
+    }
+
+    // метод (Ohtioma51)
+    void flexibility() {
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " выполняет упражнение на гибкость" << endl;
+    }
+
+    // метод (SevaSob)
+    void sambo() {
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " занимается самбо" << endl;
+    }
+
+    // метод (YoGoSee)
+    void throwstheball() {
+        cout << FIO << ", " << age << " лет, " << "из группы " << group << " кидает мяч" << endl;
+    }
 };
 
 int main() {
 
-    system("chcp 1251<nul");
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
 
-    // Ñîçäàíèå îáúåêòà
-    Student student1("Åãîðîâà Àëèñà", 19, "21318");
-
-    // Âûçîâ ìåòîäîâ
+    // Создание объекта 1
+    Student student1("Егорова Алиса", 19, "21318");
     student1.runs();
     student1.jumping();
 
-    // Ñîçäíèå îáúåêòà 2 (wernikeyd)
-    Student student2 ("Ñëþçîâ Äàíèëà", 20,"21318");
-    // Âûçîâ ìåòîäîâ äëÿ îáúåêòà 2 (wernikeyd)
+    // Создание объекта 2 (wernikeyd)
+    Student student2("Слюзов Данила", 20, "21318");
     student2.runs();
     student2.jumping();
 
-    //Ð¡Ð¾Ð·Ð´Ð°Ð½Ð¸Ðµ Ð¾Ð±ÑŠÐµÐºÑ‚Ð° 4 (Efremov)
-    Student student4 ("Ð•Ñ„Ñ€ÐµÐ¼Ð¾Ð² ÐÐ¸ÐºÐ¸Ñ‚Ð°" , 20 , "21318");
-
-    // Ð’Ñ‹Ð·Ð¾Ð² Ð¼ÐµÑ‚Ð¾Ð´Ð¾Ð² Ð´Ð»Ñ Ð¾Ð±ÑŠÐµÐºÑ‚Ð° 4
-    student4.pullUP();
-    student4.pushUP();
-    student4.runs();
-    // Ð¡Ð¾Ð·Ð´Ð°Ð½Ð¸Ðµ Ð¾Ð±ÑŠÐµÐºÑ‚Ð° 3 (Chusovv)
-    Student student3("Ð§ÑƒÑÐ¾Ð² Ð¡ÐµÑ€Ð³ÐµÐ¹ ", 20, "21318");
+    // Создание объекта 3 (Chusovv)
+    Student student3("Чусов Сергей", 20, "21318");
     student3.runs();
     student3.crouches();
     student3.jumping();
 
+    // Создание объекта 4 (Efremov)
+    Student student4("Ефремов Никита", 20, "21318");
+    student4.pullUP();
+    student4.pushUP();
+    student4.runs();
+
+    // Создание объекта 5 (Zverev)
+    Student student5("Зверев Никита", 20, "21318");
+    student5.runs();
+    student5.swims();
+    student5.jumping();
+
+    // Создание объекта 6 (SevaSob)
+    Student student6("Всеволод Соболев", 25, "21318");
+    student6.sambo();
+    student6.dances();
+
+    // Создание объекта 7 (Yashkin)
+    Student student7("Яшкин Роман", 20, "21318");
+    student7.dances();
+    student7.sings();
+
+    // Создание объекта 8 (Ohtioma51)
+    Student student8("Хамицевич Артем", 20, "21318");
+    student8.runs();
+    student8.flexibility();
+
+    // Создание объекта 9 (YoGoSe)
+    Student student9("Корытов Кирилл", 20, "21318");
+    student9.runs();
+    student9.throwstheball();
 
     return 0;
 }
-
